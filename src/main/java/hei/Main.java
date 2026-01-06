07c5915b800071698880bca31bfe2f6f929cb9bb
Rindra <hei.rindra.2@gmail.com>
2026-01-06 23:06:20 +0300

    Test: add test find ingredient pagination

--- src/main/java/hei/Main.java
+++ src/main/java/hei/Main.java
@@ -8,11 +8,16 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args){
         DataRetriever dataRetriever = new DataRetriever();
-        System.out.println("//--Get All Categories--//");
-        Dish dishById = dataRetriever.findDishById(1);
-        System.out.println(dishById);
-
+        System.out.println("//--Find Dish By Id--//");
+        System.out.println(dataRetriever.findDishById(1));
+        System.out.println(dataRetriever.findDishById(99));
         System.out.println("\n");
+
+        System.out.println("//--Find Ingredients--//");
+        System.out.println(dataRetriever.findIngredients(2,2));
+        System.out.println(dataRetriever.findIngredients(3, 5));
+        System.out.println("\n");
+
 
 
     }
