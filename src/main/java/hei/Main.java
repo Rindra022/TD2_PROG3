1fc659985c5635d0bcbdad34650466a3a2d9be94
Rindra <hei.rindra.2@gmail.com>
2026-01-06 22:14:02 +0300

    test main: add test for findDishById

--- src/main/java/hei/Main.java
+++ src/main/java/hei/Main.java
@@ -1,9 +1,18 @@
 package hei;
 
 
+import hei.model.Dish;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+    public static void main(String[] args){
+        DataRetriever dataRetriever = new DataRetriever();
+        System.out.println("//--Get All Categories--//");
+        Dish dishById = dataRetriever.findDishById(1);
+        System.out.println(dishById);
+
+        System.out.println("\n");
 
 
     }
